d803264416a125647c19ce9f772964da2fe2852c
Arya bardhan das <dasarya2002@gmail.com>
2023-05-02 11:56:24 +0530

    commit

---
+++ discord-backend/socketHandlers/newConnectionHandler.js
@@ -0,0 +1,16 @@
+const newConnectionHandler = async (socket, io) => {
+    const userDetails = socket.user;
+  
+    // serverStore.addNewConnectedUser({
+    //   socketId: socket.id,
+    //   userId: userDetails.userId,
+    // });
+  
+    // update pending friends invitations list
+    // friendsUpdate.updateFriendsPendingInvitations(userDetails.userId);
+  
+    // update friends list
+    // friendsUpdate.updateFriends(userDetails.userId);
+  };
+  
+module.exports = newConnectionHandler;
