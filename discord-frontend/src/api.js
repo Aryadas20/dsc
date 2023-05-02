5bd9c7bc03f5b13d815bf7d33f50cff15ff8d5ef
Arya bardhan das <dasarya2002@gmail.com>
2023-05-02 12:18:04 +0530

    commit 2

--- discord-frontend/src/api.js
+++ discord-frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { logout } from "./Shared/utils/auth";
+import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5002/api",
@@ -47,6 +47,41 @@ export const register = async (data) => {
 };
 
 // secure routes
+export const sendFriendInvitation = async (data) => {
+  try {
+    return await apiClient.post("/friend-invitation/invite", data);
+  } catch (exception) {
+    checkResponseCode(exception);
+    return {
+      error: true,
+      exception,
+    };
+  }
+};
+
+export const acceptFriendInvitation = async (data) => {
+  try {
+    return await apiClient.post("/friend-invitation/accept", data);
+  } catch (exception) {
+    checkResponseCode(exception);
+    return {
+      error: true,
+      exception,
+    };
+  }
+};
+
+export const rejectFriendInvitation = async (data) => {
+  try {
+    return await apiClient.post("/friend-invitation/reject", data);
+  } catch (exception) {
+    checkResponseCode(exception);
+    return {
+      error: true,
+      exception,
+    };
+  }
+};
 
 const checkResponseCode = (exception) => {
   const responseCode = exception?.response?.status;
