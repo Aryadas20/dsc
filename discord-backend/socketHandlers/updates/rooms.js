122cdd0634d89efd2655f1cbe781a3741e09185a
Arya bardhan das <dasarya2002@gmail.com>
2023-05-03 22:28:50 +0530

    complete project

--- discord-backend/socketHandlers/updates/rooms.js
+++ discord-backend/socketHandlers/updates/rooms.js
@@ -2,7 +2,7 @@ const serverStore = require("../../serverStore");
 
 const updateRooms = (toSpecifiedSocketId = null) => {
   const io = serverStore.getSocketServerInstance();
-  // const activeRooms = serverStore.getActiveRooms();
+  const activeRooms = serverStore.getActiveRooms();
 
   if (toSpecifiedSocketId) {
     io.to(toSpecifiedSocketId).emit("active-rooms", {
