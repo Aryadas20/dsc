246803279c6f34ffdec9dab5923894e0e3a122a5
Arya bardhan das <dasarya2002@gmail.com>
2023-05-03 11:31:30 +0530

    Create chatActions.js

---
+++ discord-frontend/src/store/actions/chatActions.js
@@ -0,0 +1,10 @@
+export const chatTypes = {
+    DIRECT: "DIRECT",
+    GROUP:"GROUP",
+};
+
+export const  chatActions = {
+    SET_ACTION_CHAT_DETAILS: "CHAT.SET_CHOSEN_CHAT-DETAILS",
+    SET_MESSAGES: "CHAT.SET_MESSAGES",
+    SET_CHAT_TYPE: "CHAT.SET_CHAT_TYPE",
+}
